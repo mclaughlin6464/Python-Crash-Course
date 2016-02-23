2099a69a7010195906d43a68038f648477b3d172
sean <mclaughlin6464@gmail.com>
2016-02-22 22:33:23 -0800

    first commit

---
+++ ex.py
@@ -0,0 +1,11 @@
+#Example python file
+#run me with python ex.py
+
+print "I'm the example file!"
+
+def exFunc():
+    print "I'm the example function."
+
+if __name__ == '__main__':
+    print "I'm the secret of the main funciton."
+    #run this module as main to see this secreat
